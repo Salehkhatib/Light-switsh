eb3e0d84f16a09e9ce3429a67419cb2e20b29a34
Salehkhatib <skhatib552@gmail.com>
2022-11-04 14:13:23 +0100

    new icon

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const dark_mode_button = document.getElementById("dark_mode_button")
 dark_mode_button.addEventListener("click", () =>{
     document.body.classList.toggle("dark_mode")
-    document.body.classList.toggle("second_mode")
 })
+document.querySelector("button");
+btn.addEventListener("click", function(){
+  console.log("button wurde geklickt!");
+});
+const main = document.querySelector("main");
+
+body.addEventListener("click", function (){
+  console.log("das main wurde geklickt")
+});
