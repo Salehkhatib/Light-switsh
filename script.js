afa044f451ff4c9ba964fd15b451ef199cd3e75f
Salehkhatib <skhatib552@gmail.com>
2022-11-04 15:32:23 +0100

    transition

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ const dark_mode_button = document.getElementById("dark_mode_button")
 dark_mode_button.addEventListener("click", () =>{
     document.body.classList.toggle("dark_mode")
 })
-document.querySelector("button");
-btn.addEventListener("click", function(){
-  console.log("button wurde geklickt!");
-});
-const main = document.querySelector("main");
-
-body.addEventListener("click", function (){
-  console.log("das main wurde geklickt")
-});
